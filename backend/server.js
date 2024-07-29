6ac824ad23d4cd91ea93b02d91a30f4f1b233d16
ompatil7 <ompatil4749@gmail.com>
2024-07-29 22:34:47 +0530

    finalest deploy

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ cloudinary.config({
 const allowedOrigins = [
   "http://localhost:3000",
   // "https://gamify-y1.vercel.app",
-  "https://gamify-deploy-v2.vercel.app",
+  "https://gamify-v2.vercel.app",
 ];
 
 app.use(
@@ -51,10 +51,7 @@ app.use(
 );
 app.set("trust proxy", 1);
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://gamify-deploy-v2.vercel.app"
-  );
+  res.header("Access-Control-Allow-Origin", "https://gamify-v2.vercel.app");
   res.header("Access-Control-Allow-Credentials", true);
 
   next();
