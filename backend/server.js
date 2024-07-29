3fd757842a8da0ba0cfa0d9fb16fc17f5b87c42f
ompatil7 <ompatil4749@gmail.com>
2024-07-29 22:22:56 +0530

    please god

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,8 @@ cloudinary.config({
 //middleware is a function that runs betrween req and resp
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://gamify-y1.vercel.app",
+  // "https://gamify-y1.vercel.app",
+  "https://gamify-deploy-v2.vercel.app",
 ];
 
 app.use(
@@ -50,7 +51,10 @@ app.use(
 );
 app.set("trust proxy", 1);
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://gamify-y1.vercel.app");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://gamify-deploy-v2.vercel.app"
+  );
   res.header("Access-Control-Allow-Credentials", true);
 
   next();
